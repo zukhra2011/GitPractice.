8610e391754f31d34e5918cfdd4f7bff03726765
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-07-03 10:56:18 -0400

    Added one more line.

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Adding Github Now ");
+        System.out.println("Stuff is on Github");
 
     }
 }
